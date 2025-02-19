dfdd8f2f2158902ded24e1f5d41f06ed8e243039
phillipnzau <nzauonline@gmail.com>
2025-02-19 21:03:37 +0300

    feat:assignment -lec page

--- src/app/dashboard/assignments/components/create-assignements/create-assignements.component.ts
+++ src/app/dashboard/assignments/components/create-assignements/create-assignements.component.ts
@@ -1,11 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-assignements',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './create-assignements.component.html',
-  styleUrl: './create-assignements.component.css'
+  styleUrl: './create-assignements.component.css',
 })
 export class CreateAssignementsComponent {
+  form: FormGroup;
+  currentStep = 0;
+  steps = ['Personal Info', 'Contact Details', 'Security'];
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  nextStep() {
+    this.currentStep++;
+  }
+
+  prevStep() {
+    this.currentStep--;
+  }
+
+  submitForm() {
+    if (this.form.valid) {
+      alert('Form Submitted!');
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 }
